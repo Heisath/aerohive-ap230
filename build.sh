072eb0321b929103835b38bd3265f462d3e5ae5f
Heisath <jannis@imserv.org>
2022-05-16 07:36:30 +0200

    Add lots of build system files from wdmycloud as a working base

---
+++ build.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+unalias -a
+
+# Find dir of build.sh and go there
+current_dir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+# check for whitespace in ${current_dir} and exit for safety reasons
+grep -q "[[:space:]]" <<<"${current_dir}" && { echo "\"${current_dir}\" contains whitespace. Aborting." >&2 ; exit 1 ; }
+cd "${current_dir}" || exit
+
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+BACKTITLE="AP230 kernel build script" #BACKTITLE="WDMC kernel & rootfs build script"
+
+source "${current_dir}"/inc/helpers.sh
+source "${current_dir}"/inc/kernel.sh
+#source "${current_dir}"/inc/rootfs.sh
+
+############################################################
+### (SUB) DIRECTORIES TO USE
+############################################################
+output_dir="${current_dir}/output"
+rootfs_dir="${output_dir}/rootfs"
+#boot_dir="${output_dir}/boot"
+cache_dir="${current_dir}/cache"
+
+current_user="$(stat --format %U "${current_dir}"/.git)"
+
+############################################################
+### Begin figuring out configuration
+############################################################
+GHRUNNER='off'
+THREADS=8
+EXTRA_PKGS='bash-completion htop'
+#makehelp='make CROSS_COMPILE=/usr/bin/arm-none-eabi- ARCH=arm'  
+#makehelp='make CROSS_COMPILE=/home/user/x-tools/arm-hsth-linux-gnueabi/bin/arm-hsth-linux-gnueabi- ARCH=arm LOCALVERSION='
+makehelp='make CROSS_COMPILE=/usr/bin/arm-linux-gnueabi- ARCH=arm'
+
+
+# start by reading command line arguments
+read_arguments "$@"
+
+#if [[ "${EUID}" != "0" ]] && [[ $GHRUNNER != 'on' ]]; then
+#   echo "This script requires root privileges, please rerun using sudo"
+#    exit 1
+#fi
+
+if [[ $GHRUNNER != 'on' ]]; then
+    echo "### Will try to use apt to install prerequisites."
+    apt-get install build-essential bc libncurses5 dialog u-boot-tools git libncurses-dev lib32z1 lib32ncurses5-dev libmpc-dev libmpfr-dev libgmp3-dev flex bison debootstrap debian-archive-keyring qemu-user-static gcc-arm-none-eabi
+fi
+
+# calculate dialog sizes
+exec 3>&1
+dsize=$(dialog --print-maxsize 2>&1 1>&3)
+exec 3>&-
+dsize=( $(echo "$dsize" | grep -o -E '[0-9]+') )
+DIALOG_HEIGHT=$((${dsize[0]} - 12))
+DIALOG_WIDTH=$((${dsize[1]} - 8))
+
+# if command line has selected neither kernel nor rootfs we can assume, no selection was done and ask user
+if [[ -z $BUILD_KERNEL ]] && [[ -z $BUILD_ROOTFS ]]; then
+
+    # set sensible defaults
+    BUILD_KERNEL='on'
+    CLEAN_KERNEL_SRC='on'
+    ALLOW_KERNEL_CONFIG_CHANGES='on'
+
+#    BUILD_ROOTFS='on'
+#    ALLOW_ROOTFS_CHANGES='off'
+#    ALLOW_CMDLINE_CHANGES='off'
+#    ASK_EXTRA_PKGS='off'
+#    ZRAM_ENABLED='on'
+
+    # Show user checklist to select
+    display_checklist "Build setup" "Select components and options for build:" \
+        "1" "Linux Kernel" "$BUILD_KERNEL" \
+        "2" "Clean Kernel sources" "$CLEAN_KERNEL_SRC" \
+        "3" "Allow Kernel config changes" "$ALLOW_KERNEL_CONFIG_CHANGES" \
+#        "4" "Debian Rootfs" "$BUILD_ROOTFS" \
+#        "5" "Pause to allow rootfs changes via filesystem" "$ALLOW_ROOTFS_CHANGES" \
+#        "6" "Enter bash in rootfs for manual changes" "$ALLOW_CMDLINE_CHANGES" \
+#        "7" "Ask for extra apt pkgs" "$ASK_EXTRA_PKGS" \
+#        "8" "Enable ZRAM on rootfs" "$ZRAM_ENABLED"
+
+    # Accept user choices
+    BUILD_KERNEL='off'
+    CLEAN_KERNEL_SRC='off'
+    ALLOW_KERNEL_CONFIG_CHANGES='off'
+    BUILD_ROOTFS='off'
+    ALLOW_ROOTFS_CHANGES='off'
+    ALLOW_CMDLINE_CHANGES='off'
+    ASK_EXTRA_PKGS='off'
+    ZRAM_ENABLED='off'
+
+    [[ $selection == *1* ]] && BUILD_KERNEL='on'
+    [[ $selection == *2* ]] && CLEAN_KERNEL_SRC='on'
+    [[ $selection == *3* ]] && ALLOW_KERNEL_CONFIG_CHANGES='on'
+    [[ $selection == *4* ]] && BUILD_ROOTFS='on'
+    [[ $selection == *5* ]] && ALLOW_ROOTFS_CHANGES='on'
+    [[ $selection == *6* ]] && ALLOW_CMDLINE_CHANGES='on'
+    [[ $selection == *7* ]] && ASK_EXTRA_PKGS='on'
+    [[ $selection == *8* ]] && ZRAM_ENABLED='on'
+
+else # at least kernel or rootfs has been selected via command line, check other options and set defaults
+    [[ -z $CLEAN_KERNEL_SRC  ]] && CLEAN_KERNEL_SRC='on'
+    [[ -z $ALLOW_KERNEL_CONFIG_CHANGES  ]] && ALLOW_KERNEL_CONFIG_CHANGES='off'
+
+    [[ -z $ALLOW_ROOTFS_CHANGES  ]] && ALLOW_ROOTFS_CHANGES='off' 
+    [[ -z $ASK_EXTRA_PKGS  ]] && ASK_EXTRA_PKGS='off'
+    [[ -z $ZRAM_ENABLED  ]] && ZRAM_ENABLED='on'
+fi
+
+
+# inquire about further kernel configuration
+if [[ $BUILD_KERNEL == "on" ]] && [ -z "$kernel_branch" ]; then
+    display_select "Kernel Building" "Please select the Linux Kernel branch to build." \
+         "v3.16.36" "Vendor kernel" \
+         "v4.9.311" "LK 4.9.311" \
+         "linux-5.17.y" "Linux kernel 5.17" \
+         
+#        "4.18" "Linux kernel 4.18" \
+#        "5.6" "Linux kernel 5.6" \
+#        "5.8" "Linux kernel 5.8" \
+#        "5.10" "Linux kernel 5.10" \
+#        "5.11" "Linux kernel 5.11" \
+#        "5.12" "Linux kernel 5.12" \
+#        "5.13" "Linux kernel 5.13" \
+#        "5.14" "Linux kernel 5.14" \
+#        "5.15" "Linux kernel 5.15" \
+#        "5.16" "Linux kernel 5.16" \
+#        "5.17" "Linux kernel 5.17" \
+
+    kernel_branch="$selection"
+fi
+BACKTITLE+=" | "${kernel_branch}
+
+
+# only allow building initramfs if rootfs build is enabled
+if [[ $BUILD_ROOTFS == "on" ]] && [[ -z $BUILD_INITRAMFS ]]; then
+    display_select "Build initramfs" "Do you want to build the initramfs?" \
+    "y" "yes" \
+    "n" "no"
+
+    BUILD_INITRAMFS='off'
+
+    [[ $selection == "y" ]] && BUILD_INITRAMFS='on'
+fi
+
+# get details for building the rootfs
+if [[ $BUILD_ROOTFS == "on" ]]; then
+    if [ -z "$release" ]; then
+        display_select "Rootfs creation" "Please select the Debian release to build." \
+            "buster" "Debian Buster" \
+            "bullseye" "Debian Bullseye" 
+
+        release=$selection
+    fi
+
+    if [[ "$ASK_EXTRA_PKGS" == 'on' ]]; then
+        display_input "Rootfs creation" "Type in any extra apt packages you want install. (Space seperated)" "$EXTRA_PKGS"
+        EXTRA_PKGS="$selection"
+    fi
+
+    if [[ -z "$BOOT_DEVICE" ]]; then
+        display_select "Rootfs creation" "Please select which fstab to setup?" \
+            "usb" "For usage with USB stick (boot on sdb1, root on sdb2)" \
+            "hdd" "For usage with internal HDD (boot&root on sda3, data on sda2)"
+        BOOT_DEVICE="$selection"
+    fi
+
+    if [[ "$BOOT_DEVICE" != "hdd" ]] && [[ "$BOOT_DEVICE" != "usb" ]]; then
+        display_select "Rootfs creation" "Invalid boot device selected! Please choose:" \
+            "usb" "For usage with USB stick (boot on sdb1, root on sdb2)" \
+            "hdd" "For usage with internal HDD (boot&root on sda3, data on sda2)"
+        BOOT_DEVICE="$selection"
+    fi
+
+    if [ -z "$root_pw" ]; then
+        # Adjust default root pw
+        display_input "Rootfs creation" "Type in the root password (Warning, root ssh will be enabled)" "1234"
+        root_pw="$selection"
+    fi
+
+    if [ -z "$def_hostname" ]; then
+        # Adjust hostname here
+        display_input "Rootfs creation" "Type in the hostname for the WDMC" "wdmycloud"
+        def_hostname="$selection"
+    fi
+fi
+BACKTITLE+=" | "${release}
+
+[[ $BUILD_KERNEL == "on" ]] && build_kernel
+[[ $BUILD_ROOTFS == "on" ]] && build_root_fs
---
+++ inc/kernel.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+
+build_kernel()
+{
+    # do preparation steps
+    echo "### Cloning linux kernel $kernel_branch"
+
+    if [[ $kernel_branch == *linux* ]]; then
+        kernel_dir="${cache_dir}/$kernel_branch";
+        kernel_config="config/$kernel_branch.config";
+    else
+        kernel_dir="${cache_dir}/linux-$kernel_branch";
+        kernel_config="config/linux-$kernel_branch.config";
+    fi
+    
+    if [[ $GHRUNNER == 'on' ]]; then
+        rm -rf "${output_dir}"
+    fi
+
+    # generate output directory
+    mkdir -p "${output_dir}"
+#    mkdir -p "${boot_dir}"
+
+    cleaned_or_new=0
+
+    if [ ! -d ${kernel_dir} ]; then
+        echo "### Kernel dir does not exist, cloning kernel"
+
+        mkdir -p "${kernel_dir}"
+
+        # git clone linux tree
+        git clone --branch "$kernel_branch" --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git "${kernel_dir}"
+
+        cleaned_or_new=1
+
+    else
+        if [ ${CLEAN_KERNEL_SRC} = 'on' ]; then
+            echo "### Kernel dir does exist. Fetching and cleaning"
+
+            cd ${kernel_dir}
+
+            git fetch --depth 1 origin "$kernel_branch"
+
+            git checkout -f -q FETCH_HEAD
+            git clean -qdf
+
+            cd ${current_dir}
+            
+            cleaned_or_new=1
+           
+        else
+            echo "### Kernel dir does exist. --clean not provided"
+            echo "### Continuing with dirty kernel src"
+
+        fi
+
+    fi
+
+    kernel_version=$(grab_version "${kernel_dir}");
+
+    if [[ $kernel_version == 0 ]]; then
+        rm -rf "${kernel_dir}"
+        exit_with_error "### Error cloning kernel"
+    fi
+   
+    if [[ $cleaned_or_new == 1 ]]; then
+    
+        if [[ $kernel_version == 3.* ]]; then
+            # fix lk3.16.36 yylloc bug
+            sed -i 's/YYLTYPE yylloc;/extern YYLTYPE yylloc;/' "${kernel_dir}"/scripts/dtc/dtc-lexer.lex.c_shipped
+        fi
+        
+        # copy config and dts
+        echo "### Moving kernel config in place"
+
+        if [ ! -f ${kernel_config} ]; then
+            cp config/vendor-3.16.36.config ${kernel_config}
+        fi
+
+        cp "${kernel_config}" "${kernel_dir}"/.config
+        cp dts/*.dts "${kernel_dir}"/arch/arm/boot/dts/
+
+    fi    
+   
+
+    # cleanup old modules for this kernel, this helps when rebuilding kernel with less modules
+    if [ -d "${output_dir}"/lib/modules/"${kernel_version}" ]; then
+    	rm -r "${output_dir}"/lib/modules/"$kernel_version"
+    elif [ -d "${output_dir}"/lib/modules/"${kernel_version}"+ ]; then
+    	rm -r "${output_dir}"/lib/modules/"$kernel_version"+
+    fi
+
+
+    # cd into linux source
+    cd "${kernel_dir}"
+
+    echo "### Starting make"
+
+    if [ ${ALLOW_KERNEL_CONFIG_CHANGES} = 'on' ]; then
+        $makehelp menuconfig
+    fi
+    $makehelp -j${THREADS} zImage 2>&1 | tee ${current_dir}/zImage.log
+    $makehelp -j${THREADS} ap230.dtb 2>&1 | tee ${current_dir}/dtb.log
+    cat arch/arm/boot/zImage arch/arm/boot/dts/ap230.dtb > zImage_and_dtb
+    mkimage -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n "${kernel_branch}" -d zImage_and_dtb "${output_dir}"/uImage-${kernel_version}
+    rm zImage_and_dtb
+
+    $makehelp -j${THREADS} modules 2>&1 | tee ${current_dir}/modules.log
+    $makehelp -j${THREADS} INSTALL_MOD_PATH="${output_dir}" modules_install 2>&1 | tee -a ${current_dir}/modules.log
+
+    cd "${current_dir}"
+
+    echo "### Copying new kernel config to output"
+    cp "${kernel_dir}"/.config "${output_dir}"/linux-${kernel_version}.config
+
+#    echo "### Adding default ramdisk to output"
+#    cp prebuilt/uRamdisk "${boot_dir}"
+
+    # set permissions for later runnable files
+#    chmod =rwxrxrx "${boot_dir}"/uRamdisk
+    chmod =rwxrxrx "${output_dir}"/uImage-${kernel_version}
+
+#    cp "${boot_dir}"/uImage-${kernel_version} "${boot_dir}"/uImage
+
+#    echo "### Cleanup and tar results"
+#    rm "${output_dir}"/lib/modules/*/source
+#    rm "${output_dir}"/lib/modules/*/build
+
+    # tar and compress modules for easier transport
+#    cd "${output_dir}"/lib/modules/
+#    if [ -d "${kernel_version}" ]; then
+#    	tar -czf "${output_dir}"/modules-${kernel_version}.tar.gz "${kernel_version}"
+#    elif [ -d "${kernel_version}"+ ]; then
+#    	tar -czf "${output_dir}"/modules-${kernel_version}+.tar.gz "${kernel_version}"+
+#    else
+#    	echo "### Failed to tar up modules folder! It might be missing from the output."
+#    fi
+
+    cd "${output_dir}"
+#    tar -czf "${output_dir}"/boot-${kernel_version}.tar.gz boot/uRamdisk boot/uImage-${kernel_version} boot/uImage boot/linux-${kernel_version}.config
+
+#    rm "${boot_dir}"/uImage
+
+    cd "${current_dir}"
+
+    # abort point for github runner to keep it from messing with permissions
+    if [[ $GHRUNNER == 'on' ]]; then
+        exit 0
+    fi
+
+    # fix permissions on folders for usability
+    chown "root:sudo" "${cache_dir}"
+    chown "root:sudo" "${cache_dir}"/*
+
+    chown "root:sudo" "${output_dir}"
+    chown "root:sudo" "${output_dir}"/*
+#    chown "root:sudo" "${output_dir}"/lib
+
+#    if [ -f "${output_dir}"/modules-${kernel_version}.tar.gz ]; then
+#    	chown "${current_user}:sudo" "${output_dir}"/modules-${kernel_version}.tar.gz
+#	fi
+#    if [ -f "${output_dir}"/modules-${kernel_version}+.tar.gz ]; then
+#   	    chown "${current_user}:sudo" "${output_dir}"/modules-${kernel_version}+.tar.gz
+#    fi
+
+#    chown "${current_user}:sudo" "${output_dir}"/boot-${kernel_version}.tar.gz
+
+    chmod "g+rw" "${cache_dir}"
+    chmod "g+rw" "${cache_dir}"/*
+    chmod "g+rw" "${output_dir}"
+    chmod "g+rw" "${output_dir}"/*
+#    chmod -R "g+rw" "${boot_dir}"
+#    chmod "g+rw" "${output_dir}"/lib
+
+}
